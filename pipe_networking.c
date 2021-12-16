f16b91d15d3ba7943f7f520e589dbd7633c5d918
EGuo2004 <eguo20@stuy.edu>
2021-12-16 11:48:08 -0500

    hi

--- pipe_networking.c
+++ pipe_networking.c
@@ -1,5 +1,61 @@
 #include "pipe_networking.h"
 
+/*=========================
+  server_setup
+  args:
+  creates the WKP (upstream) and opens it, waiting for a
+  connection.
+  removes the WKP once a connection has been made
+  returns the file descriptor for the upstream pipe.
+  =========================*/
+int server_setup() {
+  printf("Creating WKP\n");
+  int check = mkfifo(WKP,0644);
+  if(check == -1) {
+    printf("error: %s\n", strerror(errno));
+    return 0;
+  }
+
+  printf("Opening WKP\n");
+  int from_client = open(WKP,O_RDONLY);
+  if(from_client == -1) {
+    printf("error: %s\n", strerror(errno));
+    return 0;
+  }
+  
+  printf("Removing WKP\n");
+  remove(WKP);
+  
+  return from_client;
+  
+}
+
+/*=========================
+  server_connect
+  args: int from_client
+  handles the subserver portion of the 3 way handshake
+  returns the file descriptor for the downstream pipe.
+  =========================*/
+int server_connect(int from_client) {\
+  
+  printf("Creating and Reading Into Secret Name\n");
+  char* secret = calloc(HANDSHAKE_BUFFER_SIZE, sizeof(char));
+  read(*from_client, secret, HANDSHAKE_BUFFER_SIZE);
+
+  printf("Opening Secret\n");
+  int to_client = open(secret,O_WRONLY);
+
+  printf("Writing to ACK\n");
+  write(*to_client,ACK,sizeof(ACK));
+
+  printf("Recieving Response\n");
+  char* returnval = calloc(HANDSHAKE_BUFFER_SIZE, sizeof(char));;
+  read(from_client, returnval, HANDSHAKE_BUFFER_SIZE);
+  printf("Response:%s\n",returnval);
+  
+  return to_client;
+}
+
 
 /*=========================
   server_handshake

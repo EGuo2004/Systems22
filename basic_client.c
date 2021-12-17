770612049a44ad154b56a9701d2e77caba954873
EGuo2004 <hengguo2004@gmail.com>
2021-12-16 23:21:51 -0500

    Finally done

--- basic_client.c
+++ basic_client.c
@@ -11,13 +11,9 @@ int main(int argc, char *argv[]) {
   char line[100];
   while(1) {
     printf("What do you want?\n");
-    printf("getting info\n");
     fgets(line, BUFFER_SIZE, stdin);
-    printf("writing info\n");
     write(to_server, line, BUFFER_SIZE);
-    printf("reading info\n");
 		read(from_server,line,BUFFER_SIZE);
-    printf("printing info\n");
 		printf("Here:%s\n", line);
 
   }
--- basic_server.c
+++ basic_server.c
@@ -15,18 +15,14 @@ int main() {
 	  } else { 
 		  //child(subserver)
 		  to_client=server_connect(from_client);
-		  printf("connected\n");
 		  while(1) {
-		   printf("reading\n");
 		  if((read(from_client,line,BUFFER_SIZE)) == 0) {
 			  break;
 		  }
 		  int counter;
-		  printf("toupper\n");
 		  for(counter = 0; counter < strlen(line); counter++) {
 			  line[counter] = toupper(line[counter]);
 		  }
-		  printf("writing\n");
 		  write(to_client, line, BUFFER_SIZE);
   	      }
       }

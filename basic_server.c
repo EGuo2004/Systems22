da6aa4520e34e8841f36c0d21f9b698f404a5562
EGuo2004 <hengguo2004@gmail.com>
2021-12-16 22:26:15 -0500

    Work plz

--- basic_server.c
+++ basic_server.c
@@ -7,16 +7,23 @@ int main() {
   int from_client;
   char line[100];
   while(1) {
-	from_client = server_handshake( &to_client );
-  	while(1) {
-			if((read(from_client,line,sizeof(line))) == 0) {
-				break;
-			}
-			int counter;
-			for(counter = 0; counter < strlen(line); counter++) {
-				line[counter] = toupper(line[counter]);
-			}
-			write(to_client, line, sizeof(line));
-  	}
-  }
+	  from_client = server_setup();
+	  int f = fork();
+	  if(f) {
+		  //parent(server)
+	  } else { 
+		  //child(subserver)
+		  to_client=server_connect(from_client);
+		  while(1) {
+		  if((read(from_client,line,sizeof(line))) == 0) {
+			  break;
+		  }
+		  int counter;
+		  for(counter = 0; counter < strlen(line); counter++) {
+			  line[counter] = toupper(line[counter]);
+		  }
+		  write(to_client, line, sizeof(line));
+  	      }
+      }
+   }
 }
--- pipe_networking.c
+++ pipe_networking.c
@@ -9,23 +9,21 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_setup() {
-  printf("Creating WKP\n");
-  int check = mkfifo(WKP,0644);
-  if(check == -1) {
-    printf("error: %s\n", strerror(errno));
-    return 0;
+  int b;
+  int from_client;
+  char buffer[HANDSHAKE_BUFFER_SIZE];
+  printf("[server] handshake: making wkp\n");
+  b = mkfifo(WKP, 0600);
+  if ( b == -1 ) {
+    printf("mkfifo error %d: %s\n", errno, strerror(errno));
+    exit(-1);
   }
-
-  printf("Opening WKP\n");
-  int from_client = open(WKP,O_RDONLY);
-  if(from_client == -1) {
-    printf("error: %s\n", strerror(errno));
-    return 0;
-  }
-  
-  printf("Removing WKP\n");
+  //open & block
+  from_client = open(WKP, O_RDONLY, 0);
+  //remove WKP
   remove(WKP);
-  
+
+  printf("[server] handshake: removed wkp\n");
   return from_client;
   
 }
@@ -36,22 +34,33 @@ int server_setup() {
   handles the subserver portion of the 3 way handshake
   returns the file descriptor for the downstream pipe.
   =========================*/
-int server_connect(int from_client) {\
-  
-  printf("Creating and Reading Into Secret Name\n");
-  char* secret = calloc(HANDSHAKE_BUFFER_SIZE, sizeof(char));
-  read(*from_client, secret, HANDSHAKE_BUFFER_SIZE);
+int server_connect(int from_client) {
+    int to_client  = 0;
+    int b;
+    char buffer[HANDSHAKE_BUFFER_SIZE];
 
-  printf("Opening Secret\n");
-  int to_client = open(secret,O_WRONLY);
+  //read initial message
+  b = read(from_client, buffer, sizeof(buffer));
+  printf("[server] handshake received: -%s-\n", buffer);
 
-  printf("Writing to ACK\n");
-  write(*to_client,ACK,sizeof(ACK));
 
-  printf("Recieving Response\n");
-  char* returnval = calloc(HANDSHAKE_BUFFER_SIZE, sizeof(char));;
-  read(from_client, returnval, HANDSHAKE_BUFFER_SIZE);
-  printf("Response:%s\n",returnval);
+  to_client = open(buffer, O_WRONLY, 0);
+  //create SYN_ACK message
+  srand(time(NULL));
+  int r = rand() % HANDSHAKE_BUFFER_SIZE;
+  sprintf(buffer, "%d", r);
+
+  write(to_client, buffer, sizeof(buffer));
+  //rad and check ACK
+  read(from_client, buffer, sizeof(buffer));
+  int ra = atoi(buffer);
+  if (ra != r+1) {
+    printf("[server] handshake received bad ACK: -%s-\n", buffer);
+    exit(0);
+  }//bad response
+  printf("[server] handshake received: -%s-\n", buffer);
+
+  return from_client;
   
   return to_client;
 }
@@ -64,41 +73,41 @@ int server_connect(int from_client) {\
   Sets *to_client to the file descriptor to the downstream pipe.
   returns the file descriptor for the upstream pipe.
   =========================*/
-it server_handshake(int *to_client) {
-  printf("Creating WKP\n");
-  int check = mkfifo(WKP,0644);
-  if(check == -1) {
-    printf("error: %s\n", strerror(errno));
-    return 0;
-  }
+// int server_handshake(int *to_client) {
+//   printf("Creating WKP\n");
+//   int check = mkfifo(WKP,0644);
+//   if(check == -1) {
+//     printf("error: %s\n", strerror(errno));
+//     return 0;
+//   }
 
-  printf("Opening WKP\n");
-  int from_client = open(WKP,O_RDONLY);
-  if(from_client == -1) {
-    printf("error: %s\n", strerror(errno));
-    return 0;
-  }
+//   printf("Opening WKP\n");
+//   int from_client = open(WKP,O_RDONLY);
+//   if(from_client == -1) {
+//     printf("error: %s\n", strerror(errno));
+//     return 0;
+//   }
 
-  printf("Creating and Reading Into Secret Name\n");
-  char* secret = calloc(HANDSHAKE_BUFFER_SIZE, sizeof(char));
-  read(from_client, secret, HANDSHAKE_BUFFER_SIZE);
+//   printf("Creating and Reading Into Secret Name\n");
+//   char* secret = calloc(HANDSHAKE_BUFFER_SIZE, sizeof(char));
+//   read(from_client, secret, HANDSHAKE_BUFFER_SIZE);
 
-  printf("Removing WKP\n");
-  remove(WKP);
+//   printf("Removing WKP\n");
+//   remove(WKP);
 
-  printf("Opening Secret\n");
-  *to_client = open(secret,O_WRONLY);
+//   printf("Opening Secret\n");
+//   *to_client = open(secret,O_WRONLY);
 
-  printf("Writing to ACK\n");
-  write(*to_client,ACK,sizeof(ACK));
+//   printf("Writing to ACK\n");
+//   write(*to_client,ACK,sizeof(ACK));
 
-  printf("Recieving Response\n");
-  char* returnval = calloc(HANDSHAKE_BUFFER_SIZE, sizeof(char));;
-  read(from_client, returnval, HANDSHAKE_BUFFER_SIZE);
-  printf("Response:%s\n",returnval);
+//   printf("Recieving Response\n");
+//   char* returnval = calloc(HANDSHAKE_BUFFER_SIZE, sizeof(char));;
+//   read(from_client, returnval, HANDSHAKE_BUFFER_SIZE);
+//   printf("Response:%s\n",returnval);
   
-  return from_client;
-}
+//   return from_client;
+// }
 
 
 /*=========================
